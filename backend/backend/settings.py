903de6e507c33bb89784f87618be8bb0fb808478
newrivaladam <adamnurramadan@gmail.com>
2023-09-26 17:15:14 +0700

    Vercel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_-sqcjk$w7571zo@s(f1q(_d6w79)^_(7s3da-k!+!y6_a1^j-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -90,7 +90,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.app'
 
 
 # Database
